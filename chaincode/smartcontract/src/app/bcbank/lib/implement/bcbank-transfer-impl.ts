243eac84e4560cf3430884c2ceadbef73c771c53
杰安 陆 <lujiean@outlook.com>
2018-11-03 10:29:33 +0800

    c

--- chaincode/smartcontract/src/app/bcbank/lib/implement/bcbank-transfer-impl.ts
+++ chaincode/smartcontract/src/app/bcbank/lib/implement/bcbank-transfer-impl.ts
@@ -1,12 +1,12 @@
-import * as Yup from 'yup';
+// import * as Yup from 'yup';
 
-import { SystemError } from '../../../core/errors/system-error';
-import { QueryBuilder } from '../../../core/models';
-import { Helpers } from '../../../core/utils/helpers';
+// import { SystemError } from '../../../core/errors/system-error';
+// import { QueryBuilder } from '../../../core/models';
+// import { Helpers } from '../../../core/utils/helpers';
 // import { BcAccount, SexType } from '../../models/asset/bc-account';
-import { BcBankDocType } from '../../models/bc-doctype';
-import { QueryAllAccountParam } from '../../models/transation/bc-account-request';
-import { AllAccountRequestModel } from '../../models/transation/bc-account-request.model';
+// import { BcBankDocType } from '../../models/bc-doctype';
+// import { QueryAllAccountParam } from '../../models/transation/bc-account-request';
+// import { AllAccountRequestModel } from '../../models/transation/bc-account-request.model';
 import { BcBankTransfer } from '../bcbank-transfer';
 
 export class BcBankTransferImpl extends BcBankTransfer {
